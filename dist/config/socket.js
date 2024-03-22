1f29efbe9bd63d596d1420da894dca758cca9385
Nguyễn Trung Kiên <93828844+Trungkien03@users.noreply.github.com>
2024-03-22 14:03:51 +0700

    remove function

--- dist/config/socket.js
+++ dist/config/socket.js
@@ -1,10 +1,6 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.socketSetup = exports.io = void 0;
-const axios_1 = __importDefault(require("axios"));
 const socket_io_1 = require("socket.io");
 let io;
 const socketSetup = (server) => {
@@ -19,26 +15,25 @@ const socketSetup = (server) => {
         socket.on('private-message', ({ recipient, message }) => {
             io.to(recipient).emit('private-message', { sender: socket.id, message });
         });
-        socket.on('check-status-payment-36', async (data) => {
-            console.log(data);
-            let status = true;
-            if (!data.data || data.data.length === 0) {
-                status = false;
-            }
-            else {
-                const listIdPayment = data.data;
-                for (let index = 0; index < listIdPayment.length; index++) {
-                    const idPayment = listIdPayment[index];
-                    const response = await axios_1.default.get(`https://whalehome.up.railway.app/api/v1/payment/${idPayment}`);
-                    if (response.data.status === false) {
-                        status = false;
-                        break;
-                    }
-                }
-            }
-            console.log(status);
-            io.emit('check-status-payment-36', { data: status });
-        });
+        // socket.on('check-status-payment-36', async (data) => {
+        //   console.log(data)
+        //   let status = true
+        //   if (!data.data || data.data.length === 0) {
+        //     status = false
+        //   } else {
+        //     const listIdPayment = data.data
+        //     for (let index = 0; index < listIdPayment.length; index++) {
+        //       const idPayment = listIdPayment[index]
+        //       const response = await axios.get(`https://whalehome.up.railway.app/api/v1/payment/${idPayment}`)
+        //       if (response.data.status === false) {
+        //         status = false
+        //         break
+        //       }
+        //     }
+        //   }
+        //   console.log(status)
+        //   io.emit('check-status-payment-36', { data: status })
+        // })
         socket.on('disconnect', () => {
             console.log('Client disconnected', socket.id);
         });
