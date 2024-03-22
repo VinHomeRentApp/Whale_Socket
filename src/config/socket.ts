ba5cd9733c1b7798a2e72599683f2909227f5bd2
Nguyễn Trung Kiên <93828844+Trungkien03@users.noreply.github.com>
2024-03-22 11:13:49 +0700

    implement check-status-payment

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { Server } from 'http'
 import { Server as SocketIOServer } from 'socket.io'
 
@@ -18,7 +19,26 @@ const socketSetup = (server: Server) => {
       io.to(recipient).emit('private-message', { sender: socket.id, message })
     })
 
-    socket.on('check-status-payment', () => {})
+    socket.on('check-status-payment-36', async (data) => {
+      console.log(data)
+      let status = true
+
+      if (!data.data || data.data.length === 0) {
+        status = false
+      } else {
+        const listIdPayment = data.data
+        for (let index = 0; index < listIdPayment.length; index++) {
+          const idPayment = listIdPayment[index]
+          const response = await axios.get(`https://whalehome.up.railway.app/api/v1/payment/${idPayment}`)
+          if (response.data.status === false) {
+            status = false
+            break
+          }
+        }
+      }
+      console.log(status)
+      io.emit('check-status-payment-36', { data: status })
+    })
 
     socket.on('disconnect', () => {
       console.log('Client disconnected', socket.id)
